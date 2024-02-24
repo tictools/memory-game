1a9b4301b7a11d9ebda0681fdf651a3813e88d90
tictools <jaumevillarreal@gmail.com>
2024-02-24 22:04:33 +0100

    feat: add id query selector

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { animals } from "./data";
 import { createBoard, createMainHeading } from "./factories";
-import { sortCards } from "./helpers/sortCards";
+import { sortCards } from "./helpers";
+import { selectElementById } from "./selectors";
 import "./style.css";
 
 const cardsCollection = sortCards([...animals, ...animals]);
 
 document.addEventListener("DOMContentLoaded", () => {
-  const app = document!.querySelector<HTMLDivElement>("#app"); // '!' assert not null
+  const app = selectElementById("#app"); // '!' assert not null
 
   createMainHeading(app!); // '!' asserts that 'app' is not null or undefined
   createBoard(app!, cardsCollection);
