82d017e79c3e6f62e72720a94b7e14d732e45baf
tictools <jaumevillarreal@gmail.com>
2024-03-03 19:19:57 +0100

    feat: move cards creation to init method and remove main heading creator

--- src/factories/createMainHeading.ts
+++
@@ -1,5 +0,0 @@
-export const createMainHeading = (rootElement: HTMLDivElement) => {
-  const heading1 = document.createElement("h1");
-  heading1.textContent = "Memory Game";
-  rootElement!.appendChild(heading1);
-};
