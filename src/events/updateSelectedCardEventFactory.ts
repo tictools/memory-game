716a3b5f125a6e0d6c904f1d844e8b9fb4558071
tictools <jaumevillarreal@gmail.com>
2024-03-07 17:50:04 +0100

    feat: remove old code

--- src/events/updateSelectedCardEventFactory.ts
+++
@@ -1,8 +0,0 @@
-export const updateSelectedCardEventFactory = () => {
-  const updateSelectedCardEvent = new CustomEvent("custom::card::updated", {
-    bubbles: true,
-    cancelable: true,
-  } as CustomEventInit);
-
-  return updateSelectedCardEvent;
-};
--- src/factories/index.ts
+++
@@ -1,1 +0,0 @@
-export { createCards } from "./createCards";
