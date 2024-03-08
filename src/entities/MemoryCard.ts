0490c3ae1b852607c5970fc72e8b96a3e288a603
tictools <jaumevillarreal@gmail.com>
2024-03-08 10:00:04 +0100

    feat: add getters and format code

--- src/entities/MemoryCard.ts
+++ src/entities/MemoryCard.ts
@@ -1,29 +1,31 @@
 import type { Card } from "./types";
 
 export class MemoryCard implements Card {
-  id: number;
-  isFlipped: boolean;
-  value: string;
+  private _id: number;
+  private isFlipped: boolean;
+  private _value: string;
 
   constructor(id: number, value: string) {
-    this.id = id;
-    this.value = value;
+    this._id = id;
+    this._value = value;
     this.isFlipped = false;
   }
 
+  get id(): number {
+    return this._id;
+  }
+
+  get value() {
+    return this._value;
+  }
   createHTMLElement(): HTMLDivElement {
     const cardElement = document.createElement("div");
     cardElement.classList.add(
       "card",
       `${this.isFlipped ? "card--visible" : "card--hidden"}`
     );
-    cardElement.dataset.cardId = this.id.toString();
-    cardElement.textContent = this.isFlipped ? this.value : "";
-
-    // cardElement.addEventListener("click", () => {
-    //   Handle card click event (flip the card)
-    //   You may want to delegate this to the MemoryGame instance
-    // });
+    cardElement.dataset.cardId = this._id.toString();
+    cardElement.textContent = this.isFlipped ? this._value : "";
 
     return cardElement;
   }
